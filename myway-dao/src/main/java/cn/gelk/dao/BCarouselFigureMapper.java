f64cf9b915f01711413c52a94c702955cf310576
eagle646944325 <gelikui@126.com>
2018-05-18 12:57:37 +0800

    代码还原k/dao/BCarouselFigureMapper.java

--- myway-dao/src/main/java/cn/gelk/dao/BCarouselFigureMapper.java
+++
@@ -1,8 +0,0 @@
-package cn.gelk.dao;
-
-import cn.gelk.domain.BCarouselFigure;
-import cn.gelk.markerInterface.MyBatisMapper;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface BCarouselFigureMapper extends MyBatisMapper<BCarouselFigure> {
-}
--- myway-dao/src/main/java/cn/gelk/dao/BLeavingMessageMapper.java
+++
@@ -1,9 +0,0 @@
-package cn.gelk.dao;
-
-import cn.gelk.domain.BLeavingMessage;
-import cn.gelk.domain.TMember;
-import cn.gelk.markerInterface.MyBatisMapper;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface BLeavingMessageMapper extends MyBatisMapper<BLeavingMessage> {
-}
