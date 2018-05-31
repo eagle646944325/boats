c77994efd67db0feb6a7a8d8b9d45a27ee4a6693
eagle646944325 <gelikui@126.com>
2018-05-31 14:51:18 +0800

    微信小程序接口开发

--- myway-web/src/main/java/cn/gelk/controller/wutong/ApiWuTongController.java
+++ myway-web/src/main/java/cn/gelk/controller/wutong/ApiWuTongController.java
@@ -5,8 +5,12 @@ import cn.gelk.controller.BaseController;
 import cn.gelk.domain.*;
 import cn.gelk.property.Constants;
 import cn.gelk.service.sys.*;
+import cn.gelk.utils.AesCbcUtil;
+import cn.gelk.utils.HttpClientUtils;
 import cn.gelk.utils.MD5Util;
+import com.alibaba.fastjson.JSONObject;
 import org.apache.commons.lang3.StringUtils;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
@@ -14,14 +18,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import org.codehaus.xfire.util.Base64;
+
+import java.io.UnsupportedEncodingException;
+import java.security.AlgorithmParameters;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.Security;
+import java.security.spec.InvalidParameterSpecException;
+import java.util.*;
+
 
 /**
  * 系统管理
@@ -271,5 +289,108 @@ public class ApiWuTongController extends BaseController {
         return successResult(wUser1);
     }
     /***************************用户接口*******************************************************************/
+
+
+    /****************************************************微信小程序获取登陆信息 start********************************/
+    @RequestMapping("/wx/getSessionKeyOropenid")
+    @ResponseBody
+    public ModelMap getSessionKeyOropenid(String code,String wxspAppid,String wxspSecret) {
+        //登录凭证不能为空
+        if (code == null || code.length() == 0) {
+            return failureResult("code 不能为空");
+        }
+
+        //小程序唯一标识   (在微信小程序管理后台获取)
+//        String wxspAppid = "xxxxxxxxxxxxxx";
+//        //小程序的 app secret (在微信小程序管理后台获取)
+//        String wxspSecret = "xxxxxxxxxxxxxx";
+        //授权（必填）
+        String grant_type = "authorization_code";
+
+
+        //////////////// 1、向微信服务器 使用登录凭证 code 获取 session_key 和 openid ////////////////
+        //请求参数
+        String params = "appid=" + wxspAppid + "&secret=" + wxspSecret + "&js_code=" + code + "&grant_type=" + grant_type;
+        //发送请求
+        String sr = HttpClientUtils.sendGet("https://api.weixin.qq.com/sns/jscode2session", params);
+        return successResult(sr);
+//        //解析相应内容（转换成json对象）
+//        JSONObject json = JSONObject.parseObject(sr);
+//        //获取会话密钥（session_key）
+//        String session_key = json.get("session_key").toString();
+//        //用户的唯一标识（openid）
+//        String openid = (String) json.get("openid");
+
+
+    }
+    /**
+     * 解密用户敏感数据
+     *
+     * @param encryptedData 明文,加密数据
+     * @param iv            加密算法的初始向量
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "/wx/decodeUserInfo", method = RequestMethod.POST)
+    public ModelMap decodeUserInfo(String encryptedData, String iv,  String session_key ) {
+//
+//        Map map = new HashMap();
+
+        //登录凭证不能为空
+//        if (code == null || code.length() == 0) {
+//            map.put("status", 0);
+//            map.put("msg", "code 不能为空");
+//            return map;
+//        }
+
+        //小程序唯一标识   (在微信小程序管理后台获取)
+//        String wxspAppid = "xxxxxxxxxxxxxx";
+//        //小程序的 app secret (在微信小程序管理后台获取)
+//        String wxspSecret = "xxxxxxxxxxxxxx";
+        //授权（必填）
+//        String grant_type = "authorization_code";
+
+
+        //////////////// 1、向微信服务器 使用登录凭证 code 获取 session_key 和 openid ////////////////
+        //请求参数
+//        String params = "appid=" + wxspAppid + "&secret=" + wxspSecret + "&js_code=" + code + "&grant_type=" + grant_type;
+//        //发送请求
+//        String sr = HttpClientUtils.sendGet("https://api.weixin.qq.com/sns/jscode2session", params);
+//        //解析相应内容（转换成json对象）
+//        JSONObject json = JSONObject.parseObject(sr);
+//        //获取会话密钥（session_key）
+//        String session_key = json.get("session_key").toString();
+//        //用户的唯一标识（openid）
+//        String openid = (String) json.get("openid");
+
+        //////////////// 2、对encryptedData加密数据进行AES解密 ////////////////
+        try {
+            String result = AesCbcUtil.decrypt(encryptedData, session_key, iv, "UTF-8");
+            if (null != result && result.length() > 0) {
+//                map.put("status", 1);
+//                map.put("msg", "解密成功");
+                return successResult(result);
+//                JSONObject userInfoJSON = JSONObject.parseObject(result);
+//                Map userInfo = new HashMap();
+//                userInfo.put("openId", userInfoJSON.get("openId"));
+//                userInfo.put("nickName", userInfoJSON.get("nickName"));
+//                userInfo.put("gender", userInfoJSON.get("gender"));
+//                userInfo.put("city", userInfoJSON.get("city"));
+//                userInfo.put("province", userInfoJSON.get("province"));
+//                userInfo.put("country", userInfoJSON.get("country"));
+//                userInfo.put("avatarUrl", userInfoJSON.get("avatarUrl"));
+//                userInfo.put("unionId", userInfoJSON.get("unionId"));
+//                map.put("userInfo", userInfo);
+//                return map;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+//        map.put("status", 0);
+//        map.put("msg", "解密失败");
+        return failureResult("解密失败");
+    }
+
+    /****************************************************微信小程序获取登陆信息 end********************************/
 
 }
